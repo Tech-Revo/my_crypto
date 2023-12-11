a5308c16f1e71361e8d3b333e1d11cf60b58630f
Hancie123 <hanciewanemphago@gmail.com>
2023-12-11 22:09:20 +0545

    login UI created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',[AuthController::class,'index']);
+Route::get('login/forgot-password', [ForgotPasswordController::class, 'index']);
