0f91b177937976fdddf729e8cfbb6a5c0dcd54ea
Hancie123 <hanciewanemphago@gmail.com>
2023-12-15 13:45:54 +0545

    home page ui updated

--- resources/views/homeLayouts/nav.blade.php
+++ resources/views/homeLayouts/nav.blade.php
@@ -32,33 +32,43 @@
     <div class="header_bottom">
         <div class="container">
             <div class="row">
-                <div class="col-md-3 left_logo_design">
+                <div class="col-md-4 left_logo_design">
                     <div class="full">
                         <div class="logo">
-                            <a href="{{url('/')}}"><img class="img-responsive" src="{{ url('assets/img/logo_transparent.png') }}"
-                                    alt="#" style="width: 40%" /></a>
+                            <a href="{{ url('/') }}"><img class="img-responsive"
+                                    src="{{ url('assets/img/logo_transparent.png') }}" alt="#"
+                                    style="width: 25%" /></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-8">
                     <div class="full">
-                        <ul class="crypto_update">
-                            <li>
-                                <span class="head_cy">BTC</span>
-                                <span class="cont_cy">$ 54,220.9 <span class="update green_up"><i
-                                            class="fa fa-arrow-up"></i> 7.76%</span></span>
-                            </li>
-                            <li>
-                                <span class="head_cy">ETH</span>
-                                <span class="cont_cy">$ 1,836.5 <span class="update green_up"><i
-                                            class="fa fa-arrow-up"></i> 8.05%</span></span>
-                            </li>
-                            <li>
-                                <span class="head_cy">LTC</span>
-                                <span class="cont_cy">$ 199.80 <span class="update green_up"><i
-                                            class="fa fa-arrow-up"></i> 5.14%</span></span>
-                            </li>
-                        </ul>
+                        <!-- TradingView Widget BEGIN -->
+                        <div class="tradingview-widget-container">
+                            <div class="tradingview-widget-container__widget"></div>
+                            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+                                {
+                                    "symbols": [{
+                                            "proName": "FX_IDC:EURUSD",
+                                            "title": "EUR to USD"
+                                        },
+                                        {
+                                            "proName": "BITSTAMP:BTCUSD",
+                                            "title": "Bitcoin"
+                                        },
+                                        {
+                                            "proName": "BITSTAMP:ETHUSD",
+                                            "title": "Ethereum"
+                                        }
+                                    ],
+                                    "colorTheme": "light",
+                                    "isTransparent": true,
+                                    "showSymbolLogo": true,
+                                    "locale": "en"
+                                }
+                            </script>
+                        </div>
+                        <!-- TradingView Widget END -->
                     </div>
                 </div>
             </div>
@@ -75,7 +85,7 @@
                         <div class="menu">
                             <ul class="clearfix">
                                 <li class="active"><a href="{{ url('/') }}">Home</a></li>
-                                <li class="active"><a href="{{url('market')}}">Market</a></li>
+                                <li class="active"><a href="{{ url('market') }}">Market</a></li>
                                 <li><a href="#">Services</a></li>
                                 <li>
                                     <a href="#">Pages</a>
@@ -95,7 +105,8 @@
                                 </li>
                                 <li><a href="#">Contact</a></li>
 
-                                <li><a href="{{url('/login')}}" class="btn-danger text-light px-2 justify-content-end">login</a></li>
+                                <li><a href="{{ url('/login') }}"
+                                        class="btn-danger text-light px-2 justify-content-end">login</a></li>
 
                             </ul>
 
