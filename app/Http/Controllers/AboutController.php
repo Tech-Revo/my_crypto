3783c35846bb51030a95830b8236c859ebab63f2
Hancie123 <hanciewanemphago@gmail.com>
2023-12-17 20:16:27 +0545

    site setting added

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,8 +7,22 @@ use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    public function publicIndex(){
-        return view('about');
+    public function publicIndex($settingable_type = null, $settingable_id = null){
+
+        $site_setting = SiteSetting::where("settingable_type", $settingable_type)
+            ->where("settingable_id", $settingable_id)
+            ->get();
+        $data = [];
+        foreach ($site_setting as $item) {
+            if ($item->type == 'image') {
+                $data[$item->key] = $item->getFirstMediaUrl();
+            } else {
+                $data[$item->key] = $item->value;
+            }
+        }
+
+        
+        return view('about',compact('data'));
     }
 
     public function viewAboutUsEdit($settingable_type = null, $settingable_id = null){
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,29 +12,44 @@
     @include('homeLayouts.nav')
 
 
-   <!-- section -->
-      <section class="layout_padding theme_color_bg white_fonts ambess_section">
-         <div class="container">
+    <!-- section -->
+    <section class="layout_padding theme_color_bg white_fonts ambess_section">
+        <div class="container">
             <div class="row">
-               <div class="offset-md-1 col-md-10 text_align_center">
-                  <div class="full">
-                     <h2>About Coin Me</h2>
-                  </div>
-                  <div class="full margin_bottom_30">
-                     <p class="large_2"><strong>Trade Smarter, Trade Faster with CoinMe!</strong></p>
-                  </div>
-                  <div class="full">
-                     <p class="large_2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam earum, provident ad, porro aperiam dolore, blanditiis, nihil pariatur eius adipisci consequuntur officiis. Excepturi, nostrum? Id incidunt nesciunt officia hic distinctio nihil pariatur eius adipisci consequuntur officiis.</p>
-                     <p class="large_2">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Perspiciatis unde omnis iste natus error sit.</p>
-                  </div>
-                  <div class="full margin_top_30 margin_bottom_20">
-                     <p class="center"><a class="signup_bt read_more_bt" href="{{url('register-account')}}">Sign up</a></p>
-                  </div>
-               </div>
+                <div class="offset-md-1 col-md-10 text_align_center">
+                    <div class="full">
+                        @if (isset($data['about_title']))
+                            <h2>{{ $data['about_title'] }}</h2>
+                        @else
+                            <h2>Coin Me</h2>
+                        @endif
+
+                    </div>
+                    <div class="full margin_bottom_30">
+                        @if (isset($data['about_slogan']))
+                            <p class="large_2"><strong>{{ $data['about_slogan'] }}</strong></p>
+                        @else
+                            <p class="large_2"><strong>Trade Smarter, Trade Faster with CoinMe!</strong></p>
+                        @endif
+
+
+                    </div>
+                    <div class="full">
+                        @if (isset($data['about_message']))
+                            <p class="large_2">{!! $data['about_message'] !!}</p>
+                        @else
+                            <p class="large_2"><strong>No About Us Message Currently!</strong></p>
+                        @endif
+                    </div>
+                    <div class="full margin_top_30 margin_bottom_20">
+                        <p class="center"><a class="signup_bt read_more_bt" href="{{ url('register-account') }}">Sign
+                                up</a></p>
+                    </div>
+                </div>
             </div>
-         </div>
-      </section>
-      <!-- end section -->
+        </div>
+    </section>
+    <!-- end section -->
 
     @include('homeLayouts.footer')
 </body>
